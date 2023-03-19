7579087daa25373481a8030004a43b2e1a3c244b
SairamBadiga199 <128248834+SairamBadiga1995@users.noreply.github.com>
2023-03-19 13:32:35 +0000

    Career's website created and requirement file created.

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+companyName = "Sairam's"
+JOBS = [
+        {
+          'id':1,
+          'title': 'Data Analyst',
+          'location':'Bangalore, India',
+          'salary': 'Rs 1000000'    
+        },
+        {
+          'id':2,
+          'title': 'Data Scientist',
+          'location':'Delhi, India',
+          'salary': 'Rs 1000000'    
+        },
+        {
+          'id':3,
+          'title': 'Data Engineer',
+          'location':'Remote',
+          'salary': 'Rs 1000000'    
+        },
+        {
+          'id':4,
+          'title': 'Frontend Engineer',
+          'location':'San fransico, usa',
+          'salary': '$ 100000'    
+        },
+]
 
 @app.route('/')
-def hello_world():
-  return "Hello World"
+def About_Me():
+  return render_template('home.html', jobs=JOBS, companyname = companyName)
 
 if(__name__)=="__main__":
   app.run("0.0.0.0",debug = True)
